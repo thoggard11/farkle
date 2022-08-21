99ddc6ee48ce09b9bf7d6f49a4a5b54d726c1aea
Taylor Hoggard <thoggard@rossvideo.com>
2022-08-21 13:30:50 -0400

    Installed SCSS and converted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./components/Counter";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -9,7 +9,7 @@ import {
   incrementIfOdd,
   selectCount,
 } from "../redux/counterSlice";
-import styles from "./Counter.module.css";
+import styles from "./Counter.module.scss";
 
 export function Counter() {
   const count = useTypedSelector(selectCount);
